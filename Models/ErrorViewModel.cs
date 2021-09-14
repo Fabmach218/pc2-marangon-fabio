eb290d110957ebbc218fc5e7a7b27f64c19af8d0
Fabio Marangon <fabio_marangon@usmp.pe>
2021-09-14 17:18:44 -0500

    Agregando archivos de proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pc2_marangon_fabio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
